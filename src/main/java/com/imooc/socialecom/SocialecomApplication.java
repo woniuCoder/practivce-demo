5b8d76c7e47a9fb8d5bdd82abff373de20df05e7
woniuCoder <2386747369@qq.com>
2022-12-11 15:06:05 +0800

    delete info

--- src/main/java/com/imooc/socialecom/SocialecomApplication.java
+++ src/main/java/com/imooc/socialecom/SocialecomApplication.java
@@ -1,22 +1,14 @@
 package com.imooc.socialecom;
 
 import com.imooc.socialecom.config.IDGenerator;
-import com.imooc.socialecom.converter.Properties;
 import com.imooc.socialecom.enums.IDTypeEnum;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.util.StreamUtils;
-import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 @MapperScan("com.imooc.socialecom.mapper")
